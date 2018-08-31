139f63dd2125828db0cc1999712da7e1a4755879
yinyb <905330018@qq.com>
2018-08-31 14:36:08 +0800

    利用logger打印完整的okhttp网络请求和响应所有相关信息（请求行、请求头、请求体、响应行、响应行、响应头、响应体）。

--- app/src/main/java/com/yonbor/bettermvp/api/HttpApi2.java
+++ app/src/main/java/com/yonbor/bettermvp/api/HttpApi2.java
@@ -91,7 +91,7 @@ public class HttpApi2 {
                 // 以{}或者[]形式的说明是响应结果的json数据，需要进行格式化
                 if ((message.startsWith("{") && message.endsWith("}"))
                         || (message.startsWith("[") && message.endsWith("]"))) {
-                    message = JsonUtil.formatJson(message);
+                    message = JsonUtil.formatJson(JsonUtil.decodeUnicode(message));
                 }
                 mMessage.append(message.concat("\n"));
                 // 请求或者响应结束，打印整条日志
